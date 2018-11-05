db3612666f4f450c16a3f5094dc81b354d976300
Eve Redero <eve.redero@devialet.com>
2018-11-05 18:06:38 +0100

    [I2C] Changes to repo

--- C232HM_I2C/I2C_MCP45/src/compile_and_run.bash
+++ C232HM_I2C/I2C_MCP45/src/compile_and_run.bash
@@ -28,31 +28,13 @@ cp -f ../include/linux/*.h .
 cp -f ../lib/linux/i386/* .
 echo ------------------------------------------------
 echo Building sample by linking to static library and running it
-gcc -g -Wl,--no-as-needed -ldl -I. -o sample-static.o sample-static.c libMPSSE.a
-./sample-static.o
-cd ..
-}
-
-I2C-Dynamic()
-{
-cd I2C
-rm -rf libMPSSE.so
-rm -rf libMPSSE.a
-rm -rf sample-dynamic.o
-rm -rf sample-static.o
-cp -f ../include/libMPSSE_i2c.h .
-cp -f ../include/linux/*.h .
-cp -f ../lib/linux/i386/* .
-echo ------------------------------------------------
-echo Building sample by linking to dynamic library and running it
-gcc -Wl,--no-as-needed -ldl sample-dynamic.c -o sample-dynamic.o
-./sample-dynamic.o
+gcc -g -Wl,--no-as-needed -ldl -I. -o mcp45.o mcp45.c libMPSSE.a
+./mcp45.o
 cd ..
 }
 
 #Execution starts from here
 getArch ;
-#Select input
 I2C-Static;
 #Done
 
