22a640be65a85db0f0959ee50896fd3a66fd9ddd
Eve Redero <eve.redero@devialet.com>
2019-01-18 12:02:01 +0100

    [BTTEST] Change test with audio-only, no more BT ping

--- bluespeakers_test/bluetest.py
+++ bluespeakers_test/bluetest.py
@@ -9,16 +9,16 @@ import os, shutil
 import csv
 
 #Measurement parameters
-macs = ["04:52:C7:F9:43:38", "00:12:6F:B6:8D:BC", "88:C6:26:8C:0B:B1", "88:C6:26:EC:FC:D4", "7C:96:D2:04:F5:4B"]
-devices = ["Bose", "Beoplay",  "Megaboom", "Boom", "Denon"]
-record_time = 5
-sleep_time = 15
+mac = "04:52:C7:F9:43:38"
+device = "Bose"
+record_time = 2
+sleep_time = 5
 
 
 def message(device, mac_device, msg):
     now = time.time() - start_time
     with open("result.txt", "a") as myres:
-        myres.write("{:.0f}s, device {} ({}): {}\n".format(now, device, 
+        myres.write("{:.0f}s, device {} ({}): {}\n".format(now, device,
                     mac_device, msg))
 
 def write_values(val_list):
@@ -32,12 +32,12 @@ def disconnect(device, mac_device):
         stop_time = time.time() - start_time
         with open("result.txt", "a") as myres:
             print("{} disconnected !".format(device))
-            myres.write("Stop time for {} ({}):\n".format(device, 
+            myres.write("Stop time for {} ({}):\n".format(device,
                             mac_device))
             myres.write("{:.0f} seconds\n".format(stop_time))
-            myres.write("{:.0f}h {:.0f}m {:.0f}s\n".format(stop_time/3600, 
+            myres.write("{:.0f}h {:.0f}m {:.0f}s\n".format(stop_time/3600,
                         (stop_time % 3600)/60, stop_time%60))
-            myres.write("\n\n")    
+            myres.write("\n\n")
 
 def runcmd(cmd):
     sp = subprocess.Popen(cmd, shell=True,
@@ -52,7 +52,6 @@ def runcmd(cmd):
 start_time = time.time()
 with open("result.txt", "a") as myres:
      myres.write("***\nStart time: {}\n".format(time.ctime(start_time)))
-cmd_bt = "timeout -k 2 1 /usr/bin/hcitool con | grep -c {}"
 cmd_rec = "timeout -k {} {} arecord -d {} -c 1 -r 8000 -f S16_LE {}"
 wavfilename_spec = "sounds/{}{}.wav"
 idx = 0
@@ -64,30 +63,26 @@ if not os.path.exists("sounds"):
     os.mkdir("sounds")
 
 
-write_values(devices)
+write_values([device, mac])
+values = []
+n_nok = 0
 while 1:
     idx += 1
-    if len(devices) < 1:
+    wavfilename = wavfilename_spec.format(device, idx)
+    ret = runcmd(cmd_rec.format(record_time + 5, record_time + 2, record_time, wavfilename))
+    # TODO test if this fails ?
+    try:
+        is_ok, value = audio_common.compute_signal(wavfilename, threshold=0.05, nb_chan=1, log="")
+        values.append(value)
+        if not is_ok:
+            message(device, mac, "Signal not found: value {:.2f} inferior to threshold".format(value))
+            n_nok +=1
+        else:
+            n_nok = 0
+    except:
+        print("Failed audio computing")
+    if n_nok > 5:
+        message(device, mac, "No signal for 5 times, exiting...".format(value))
         break
-    for device, mac_device in zip(devices, macs):
-        ret = runcmd(cmd_bt.format(mac_device))
-        if int(ret) < 1:
-            disconnect(device, mac_device)                
-            devices.remove(device)
-            macs.remove(mac_device)
-            write_values(devices)
-    
-    values = []
-    for device, mac_device in zip(devices, macs):
-        wavfilename = wavfilename_spec.format(device, idx)
-        ret = runcmd(cmd_rec.format(record_time + 5, record_time + 2, record_time, wavfilename))
-        # TODO test if this fails ?
-        try:
-            is_ok, value = audio_common.compute_signal(wavfilename, threshold=0.05, nb_chan=1, log="")
-            values.append(value)
-            if not is_ok:
-                message(device, mac_device, "Signal not found: value {:.2f} inferior to threshold".format(value))
-        except:
-            print("Failed audio computing")
     write_values(values)
     time.sleep(sleep_time)
