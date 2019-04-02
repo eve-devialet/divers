0c8b1d68647190101436086a2facfa5ecd4c9a60
Eve Redero <eve.redero@devialet.com>
2019-04-02 15:25:35 +0200

    [AMPS] Update TAS2770 scripts with dump from software

--- amps/tas2770.py
+++ amps/tas2770.py
@@ -3,8 +3,11 @@ from tas5825m_header import *
 import subprocess as sp
 import logging
 logger = logging.getLogger(__name__)
+import tas2770_dump as td
 
 TAS2770_PWR_CTL_ADDR = 0x02
+TAS2770_PAGE_ADDR = 0x00
+TAS2770_BOOK_ADDR = 0x7F
 
 def i2c_read_reg(device=0x41, register=0x2e, mode='b'):
     cmd = "sudo i2cget -y 1 0x{:02x} 0x{:02x} {}".format(device, register, mode)
@@ -27,17 +30,30 @@ def i2c_write_reg(device=0x41, register=0x2e, value="0x00", mode='b'):
     else:
         raise IOError("No I2C communication")
 
-def tas5825m_init(device):
-    print("TAS5825M init")
+def tas2770_init(device):
+    print("TAS2770 init")
+    # Only book 0
+    i2c_write_reg(device, TAS2770_BOOK_ADDR, 0x00)
+    for page, vals in enumerate(td.dump):
+        # Change page
+        i2c_write_reg(device, TAS2770_PAGE_ADDR, page)
+        for value in vals:
+            i2c_write_reg(device, value[0], value[1])
 
-    # Select page 0
-    i2c_write_reg(device, 0, 0)
-
+def tas2770_unmute(device):
+    print("TAS2770 unmute")
     # Unmute and make current and voltage sense active
     i2c_write_reg(device, TAS2770_PWR_CTL_ADDR, 0x00)
 
 if __name__ == "__main__":
     device = 0x41
+    # Only book 0
+    i2c_write_reg(device, TAS2770_BOOK_ADDR, 0x00)
+    for page, vals in enumerate(td.dump):
+        # Change page
+        i2c_write_reg(device, TAS2770_PAGE_ADDR, page)
+        for value in vals:
+            i2c_write_reg(device, value[0], value[1])
 
-
+    tas2770_unmute(device)
 
