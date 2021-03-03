47cbb45c7568844b0ff69cee5f01d1095bebc0d6
Eve Redero <eve.redero@devialet.com>
2021-03-03 18:54:25 +0100

    [DIABLO] Add volume to play script

--- bringup_tools/agma/play_sound.sh
+++ bringup_tools/agma/play_sound.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 FILENAME=$1
+USER_VOLUME=$2
 
+echo "Usage: nohub bash play_sound.sh [absolute filename] [[volume]]"
+echo "Do not forget nohup, or script will stop when adb session ends"
+echo "If relative filename is given, loop will only work once."
+echo ""
 if [[ -z $FILENAME ]];
 then
     echo `date`" - Missing mandatory argument: filename (absolute path). "
-    echo `date`" - Usage: ./play_sound.sh [filename]"
+    echo `date`" - Usage: ./play_sound.sh [filename] [[volume]]"
   exit 1
+fi
+if [[ -z $USER_VOLUME ]];
+then
+    echo `date`" - No volume selected, using 0.2. "
+    export VOLUME=0.2
+    echo `date`" - Usage: ./play_sound.sh [filename] [[volume]]"
+else
+    export VOLUME=$USER_VOLUME
 fi
 
 while true; do
